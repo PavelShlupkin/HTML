976ef97c170a23e0628f9e7800358251631dc845
PavelShlupkin <PavelShlupkin@users.noreply.github.com>
2019-05-18 01:07:07 +0400

    js hm

---
+++ js/9.1/script.js
@@ -0,0 +1,23 @@
+function formSubmit() {
+    var input = document.getElementById("input");
+    var output = document.getElementById("output");
+    output.value = CharRemover(input.value);
+
+
+    function CharRemover(str) {
+        str.split(" ").forEach(function (t) {
+            for (var i = 0; i < t.length; i++) {
+                if (t.lastIndexOf(t[i]) !== t.indexOf(t[i])) {
+                    while(str.indexOf(t[i]) !== -1 && t[i] !== "!" &&
+                    t[i] !== "." &&
+                    t[i] !== "," &&
+                    t[i] !== ";" &&
+                    t[i] !== "?" &&
+                    t[i] !== ":")
+                        str = str.replace(t[i],"")
+                }
+            }
+        });
+        return str;
+    }
+}
